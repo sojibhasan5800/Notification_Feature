44c81db9a44d5479fb44ab0c5830c3f739c2c507
phitron-demo <sojibhasan5800@gmail.com>
2025-09-11 07:14:13 +0600

    Fix app import path: updated urls.py to use 'notifications_app' instead of 'notification_app'

--- notification_sys/settings.py
+++ notification_sys/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notifications_app'
+    'notifications_app',
 ]
 
 MIDDLEWARE = [
--- notifications_app/views.py
+++ notifications_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from channels.layers import get_channel_layer
+import json
 # Create your views here.
+from django.template import RequestContext
+def home(request):
+    return render(request, 'index.html', )
+
+# from asgiref.sync import async_to_sync
+# def test(request):
+#     channel_layer = get_channel_layer()
+#     async_to_sync(channel_layer.group_send)(
+#         "notification_broadcast",
+#         {
+#             'type': 'send_notification',
+#             'message': json.dumps("Notification")
+#         }
+#     )
+#     return HttpResponse("Done")
